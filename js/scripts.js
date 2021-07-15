54c3416dd667eeac69bdeecfd358f73d84296acf
Melvin Omega <melvinomega151@gmail.com>
2021-07-15 15:23:50 +0300

    update html

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+var mass=function(height, weight) {
+    return height / weight 
+    };
+    var height = parseInt(prompt("what is your height?"));
+    var weight = parseInt(prompt("what is your weight?"));
+    alert(mass(height, weight));
